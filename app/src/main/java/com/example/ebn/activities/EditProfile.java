ffd9ffe611df15795162dbb945886eb1398fc64f
baohng <bao.hng221b@gmail.com>
2023-04-10 10:45:20 +0700

    add back transactions

--- app/src/main/java/com/example/ebn/activities/EditProfile.java
+++ app/src/main/java/com/example/ebn/activities/EditProfile.java
@@ -2,15 +2,36 @@ package com.example.ebn.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.example.ebn.R;
+import com.example.ebn.fragments.AccountFragment;
 
-public class EditProfile extends AppCompatActivity {
+public class EditProfile extends AppCompatActivity implements View.OnClickListener {
+
+
+    private ImageView backToIcon;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+
+        backToIcon = findViewById(R.id.backToIcon);
+        backToIcon.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.backToIcon:
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
